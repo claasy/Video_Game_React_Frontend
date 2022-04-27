aa8ee0cca2fda935262b4f9a036d8edc0a06aec9
Amanda <aclaas522@gmail.com>
2022-04-27 16:21:44 -0500

    Cleaned Up Graphs Components

--- src/Components/GameGraphs/GameGraphs.jsx
+++ src/Components/GameGraphs/GameGraphs.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Chart from "react-google-charts";
 
 
-
 const GameGraphs = (props) => {
     const [data,setData] = useState([]);
 
@@ -19,21 +18,49 @@ const GameGraphs = (props) => {
 
         
         let platformArrays = distinctPlatform.map(platform => {
-            // return [platform, 10, "silver"]
             let total = 0 
             filterGames.map(el=>{
                 if(el.platform == platform){
                     total+= el.globalSales
                 }
             })
-            // let sales = filterGames.reduce((total,game) => {
-              
-            //     if(game.platform == platform){
-            //         return total + game.globalSales
-            //     }
-            // },0)
+           
             return [platform, total, "silver"]
-        //     let allGamesForPlatform = filterGames.filter( game => game.platform == platform)
+      
+        });
+       
+        const dataValue = [
+            ["Platform", "Total sales", { role: "style" }],
+         ...platformArrays
+        ];
+        console.log(dataValue)
+        setData(dataValue)
+    
+    }
+  
+    useEffect(()=>{
+        generateGraph()
+    },[])
+   
+
+    return ( 
+        <div>
+            <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
+        </div>
+     
+            )
+ 
+}
+ 
+export default GameGraphs;
+
+
+
+   // props.data.filter(info => info.year > 2013).map((data, index) => {
+        //     console.log(data)
+        //     return (
+
+  //     let allGamesForPlatform = filterGames.filter( game => game.platform == platform)
         //     for (let i = 0; i < allGamesForPlatform.length; i++){
         //         if (obj[i.platform] != undefined) {
         //             i.platform += i.globalSales
@@ -44,43 +71,6 @@ const GameGraphs = (props) => {
         //    if (obj[platform] != undefined){
         //        return [platform, obj.platform, "silver"]
         //    }
-        });
-       
-     
-       
-       
-        const dataValue = [
-            ["Platform", "Total sales", { role: "style" }],
-         ...platformArrays
-        ];
-        console.log(dataValue)
-        setData(dataValue)
-    
-    }
-   
-  
-    useEffect(()=>{
-        generateGraph()
-    },[])
-   
-
-  
-    return ( 
-        <div>
-            <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
-        </div>
-        // props.data.filter(info => info.year > 2013).map((data, index) => {
-        //     console.log(data)
-        //     return (
-               
-            )
-    //     })
-    //  );
-}
- 
-export default GameGraphs;
-
-
 
      // let allGamesForPlatform = filterGames.filter(game => game.platform == platform);
         // let content = []
